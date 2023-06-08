c5d1c8ecf9e7309b635bbfaa1018a9212a0b4804
Utsav Patel <utsavpatel00265@gmail.com>
2023-06-08 22:19:27 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Utsav Patel on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
